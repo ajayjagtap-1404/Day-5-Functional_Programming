306f921156f659a87f4d6764342e3da40aa1f69a
ajayjagtap-1404 <ajayjagtap1404@gmail.com>
2023-03-01 00:38:17 +0530

    Added : code for check distance using Euclidean distance formula

--- FunctionalProgramming.java
+++ FunctionalProgramming.java
@@ -4,50 +4,11 @@ import java.util.Scanner;
 
 public class FunctionalProgramming {
     Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Functional Programming :");
-
-        SumOfThreeIntegers sumOfThreeIntegers = new SumOfThreeIntegers();
-        System.out.println("Enter Array size : ");
-        int size = sumOfThreeIntegers.sc.nextInt();
-        int [] arr = new int[size];
-        sumOfThreeIntegers.getArrInput(size,arr);
-
-        sumOfThreeIntegers.checkTriplets(arr);
-
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        double distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+        System.out.println("Distance from (0, 0) to (" + x + ", " + y + ") is " + distance);
     }
-
-    private void checkTriplets( int[] arr) {
-        System.out.println("Triplets are : ");
-        int count = 0;
-        for (int i =0; i<arr.length; i++) {
-            for (int j =i+1; j< arr.length; j++) {
-                for (int k=j+1; k< arr.length; k++) {
-                    int sum = arr[i]+arr[j]+arr[k];
-
-                    if (sum == 0) {
-                        System.out.println( + arr[i] +" "+ arr[j]+" "+arr[k]);
-                        count++;
-                    }
-                }
-            }
-        }
-        System.out.println("Count of Triplet pair : " +count);
-    }
-
-    private   void getArrInput(int size, int []arr) {
-
-        for (int i=0; i<size; i++) {
-            System.out.println("Enter " +(i+1) + " element :");
-            arr[i] = sc.nextInt();
-        }
-        System.out.println("Element are : ");
-        for (int i=0 ; i< arr.length ; i++) {
-            System.out.print(arr[i]+ ",");
-
-        }
-        System.out.println();
-
-         }
-    }
-
+}
