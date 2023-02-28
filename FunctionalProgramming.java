5db65c71b584a6f3615fbd4b69f83dc01b808df2
ajayjagtap-1404 <ajayjagtap1404@gmail.com>
2023-03-01 00:12:30 +0530

    Added : code for showing welcome msg

---
+++ FunctionalProgramming.java
@@ -0,0 +1,7 @@
+package com.assigenement.day_5.functional_programming;
+
+public class FunctionalProgramming {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Functional Programming :");
+    }
+}
